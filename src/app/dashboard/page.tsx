295a5bb083efb3e901dd82d2804c2a11e3545fb8
AmrTamer23 <amrtamer23mj@gmail.com>
2024-09-26 07:31:31 +0300

    feat: implement generic dashboard view

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import { ContentLayout } from "@/components/content-layout";
+
+export default function DashboardPage() {
+  return (
+    <ContentLayout title="Dashboard">This is the dashboard page.</ContentLayout>
+  );
+}
