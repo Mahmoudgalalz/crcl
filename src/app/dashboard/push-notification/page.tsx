24ebb6ab03aa8aca3b12b820829267f365fe40f1
AmrTamer23 <amrtamer23mj@gmail.com>
2024-09-26 16:20:16 +0300

    feat: sidebar with all pages routes ready to implement

---
+++ src/app/dashboard/push-notification/page.tsx
@@ -0,0 +1,9 @@
+import { ContentLayout } from "@/components/content-layout";
+
+export default function PushNotificationsPage() {
+  return (
+    <ContentLayout title="Push Notifications">
+      This is the Push Notifications page
+    </ContentLayout>
+  );
+}
---
+++ src/app/dashboard/tickets-reqs/page.tsx
@@ -0,0 +1,9 @@
+import { ContentLayout } from "@/components/content-layout";
+
+export default function TicketsReqsPage() {
+  return (
+    <ContentLayout title="Tickets Requests">
+      This is the Tickets Requests page
+    </ContentLayout>
+  );
+}
