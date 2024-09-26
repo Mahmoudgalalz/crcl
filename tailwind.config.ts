e2109131ae472f6e76ad5b781610deb333e9efbd
AmrTamer23 <amrtamer23mj@gmail.com>
2024-09-26 23:52:53 +0300

    chore: add the orange theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,40 +15,51 @@ const config: Config = {
     screens,
     extend: {
       colors: {
-        main: "#E1D1C5",
-        mainAccent: "#C6AFA0",
-        overlay: "rgba(0,0,0,0.5)",
-
-        background: "#f8f9fa",
-        foreground: "#343a40",
-        destructive: "#dc2626",
-        "destructive-foreground": "#ffffff",
-
-        darkBg: "#343a40",
-        darkText: "#f8f9fa",
-        darkBorder: "#495057",
-        secondaryBlack: "#212529",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        chart: {
+          "1": "hsl(var(--chart-1))",
+          "2": "hsl(var(--chart-2))",
+          "3": "hsl(var(--chart-3))",
+          "4": "hsl(var(--chart-4))",
+          "5": "hsl(var(--chart-5))",
+        },
       },
       borderRadius: {
-        base: "5px",
-      },
-      boxShadow: {
-        light: "4px 4px 0px 0px #000",
-        dark: "4px 4px 0px 0px #000",
-      },
-      translate: {
-        boxShadowX: "4px",
-        boxShadowY: "4px",
-        reverseBoxShadowX: "-4px",
-        reverseBoxShadowY: "-4px",
-      },
-      fontWeight: {
-        base: "500",
-        heading: "700",
-      },
-      fontFamily: {
-        sans: "var(--font-geist-sans)",
-        mono: "var(--font-geist-mono)",
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
     },
   },
