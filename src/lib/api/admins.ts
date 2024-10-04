d476d54ab60dfdb288f861e24589582ec909570c
AmrTamer23 <amrtamer23mj@gmail.com>
2024-10-04 07:04:18 +0300

    feat: implement admin reset password func

--- src/lib/api/admins.ts
+++ src/lib/api/admins.ts
@@ -26,3 +26,18 @@ export async function deleteAdmin(id: string) {
     };
   }
 }
+
+export async function changePasswordAdmin(id: string, newPassword: string) {
+  try {
+    const response = await axiosInstance.patch<SuperUser>(`/admin/${id}`, {
+      password: newPassword,
+    });
+    const data = response.data;
+    return data;
+  } catch (error) {
+    console.error(error);
+    return {} as unknown as {
+      event: SuperUser;
+    };
+  }
+}
