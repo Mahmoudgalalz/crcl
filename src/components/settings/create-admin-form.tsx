51d081b38be1b06a22adc04fb7fb6a5f4e048537
AmrTamer23 <amrtamer23mj@gmail.com>
2024-10-04 06:15:25 +0300

    feat: init settings page ui and implemented display admins func

---
+++ src/components/settings/create-admin-form.tsx
@@ -0,0 +1,90 @@
+"use client";
+import { Plus } from "lucide-react";
+import { Button } from "../ui/button";
+import { FormField, FormItem, FormLabel, FormControl, Form } from "../ui/form";
+import { Input } from "../ui/input";
+import { useToast } from "@/hooks/use-toast";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+  name: z.string().min(2).max(50),
+  email: z.string().email(),
+  password: z.string().min(6),
+});
+
+type FormValues = z.infer<typeof formSchema>;
+
+export function CreateAdminForm() {
+  const { toast } = useToast();
+
+  const form = useForm<FormValues>({
+    resolver: zodResolver(formSchema),
+  });
+
+  async function onSubmit(data: FormValues) {
+    toast({
+      title: "Success",
+      description: "Admin created successfully",
+    });
+  }
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <div className="grid w-full items-center gap-4">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter admin name" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    type="email"
+                    placeholder="Enter admin email"
+                    {...field}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input
+                    type="password"
+                    placeholder="Enter admin password"
+                    {...field}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+        </div>
+        <Button className="mt-4" type="submit">
+          <Plus className="mr-2 h-4 w-4" />
+          Create Admin
+        </Button>
+      </form>
+    </Form>
+  );
+}
