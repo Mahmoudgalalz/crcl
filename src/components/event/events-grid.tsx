62a3c6a285b86f54808b752595f2e75645f5ba7d
AmrTamer23 <amrtamer23mj@gmail.com>
2024-10-08 06:20:33 +0300

    chore: sort events by status and update mutated event in the events page

--- src/components/event/events-grid.tsx
+++ src/components/event/events-grid.tsx
@@ -7,9 +7,22 @@ export function EventsGrid({ events }: { events?: AnEvent[] }) {
   return (
     <div className="container mx-auto py-10">
       <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-        {events?.map((event) => (
-          <EventItem event={event} key={event.id} />
-        ))}
+        {events
+          ?.sort((a, b) => {
+            const statusOrder = [
+              "DRAFTED",
+              "PUBLISHED",
+              "ENDED",
+              "CANCLED",
+              "DELETED",
+            ];
+            return (
+              statusOrder.indexOf(a.status) - statusOrder.indexOf(b.status)
+            );
+          })
+          .map((event) => (
+            <EventItem event={event} key={event.id} />
+          ))}
       </div>
     </div>
   );
