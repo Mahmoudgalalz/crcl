665978455b30af9337ff42a0ca760c7c72821e1a
AmrTamer23 <amrtamer23mj@gmail.com>
2024-10-25 02:19:48 +0300

    chore: fix hydration errors

--- src/components/settings/ops-table.tsx
+++ src/components/settings/ops-table.tsx
@@ -87,7 +87,7 @@ export function OpsTable() {
                 </div>
               </TableCell>
             </TableRow>
-          ))}{" "}
+          ))}
         </TableBody>
       </Table>
     </CardContent>
