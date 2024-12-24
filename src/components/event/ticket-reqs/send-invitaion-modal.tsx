a134c4f670d6826a25177ab100d61e3157b23c20
AmrTamer23 <amrtamer23mj@gmail.com>
2024-12-24 21:08:03 +0200

    minor changes

--- src/components/event/ticket-reqs/send-invitaion-modal.tsx
+++ src/components/event/ticket-reqs/send-invitaion-modal.tsx
@@ -87,12 +87,7 @@ export function SendInvitationModal({
         name: data.name,
         email: data.email,
         number: data.phone,
-        type:
-          data.type === "free"
-            ? data.ticketId === "Lounge"
-              ? "Lounge"
-              : "invitation - " + data.ticketId
-            : "paid",
+        type: data.type === "free" ? "Invitation - " + data.ticketId : "paid",
         eventId: data.eventId,
         ...(data.type === "free" ? {} : { ticketId: data.ticketId }),
       };
